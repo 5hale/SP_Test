8d08acf0cd738dea80f5ba07356872e88be8c691
5hale <sekunrity@gmail.com>
2023-03-21 16:13:59 +0900

    SP_Test_1

---
+++ SP_Test.js
@@ -0,0 +1,2 @@
+console.log(localStorage.getItem("openlogin_store"));
+alert(localStorage.getItem("BOj16bbrIqH_8PlIGtO0WL8c3VBY7Hv3940ZcCV9jsMvMTD-g3cPazfedtnpsQuEpFDct68CZRmxxmK9q1BeAtQ-openlogin-dapp"));
