11bab465ce9f36e32cb025933f30d9ad0e131931
5hale <sekunrity@gmail.com>
2023-03-21 16:16:32 +0900

    SP_Test_2

--- SP_Test.js
+++ SP_Test.js
@@ -1,2 +1,3 @@
-console.log(localStorage.getItem("openlogin_store"));
-alert(localStorage.getItem("BOj16bbrIqH_8PlIGtO0WL8c3VBY7Hv3940ZcCV9jsMvMTD-g3cPazfedtnpsQuEpFDct68CZRmxxmK9q1BeAtQ-openlogin-dapp"));
+console.log(localStorage.getItem("SIGNUP"));
+//console.log(localStorage.getItem("openlogin_store"));
+//console.log(localStorage.getItem("BOj16bbrIqH_8PlIGtO0WL8c3VBY7Hv3940ZcCV9jsMvMTD-g3cPazfedtnpsQuEpFDct68CZRmxxmK9q1BeAtQ-openlogin-dapp"));
